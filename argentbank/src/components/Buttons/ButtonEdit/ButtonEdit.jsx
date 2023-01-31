13485077f3aeb60d4e2e95b186accb4b03d7b39d
Aude Alexandre <alexandre.aude16@gmail.com>
2023-01-31 15:49:42 +0100

    Déplacement dossier hors back

--- argentbank/src/components/Buttons/ButtonEdit/ButtonEdit.jsx
+++
@@ -1,5 +0,0 @@
-import styles from './ButtonEdit.module.scss';
-
-export function ButtonEdit({ children }) {
-  return <button className={styles.button}>{children}</button>;
-}
--- argentbank/src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Footer.module.scss';
-
-export function Footer() {
-  return (
-    <div className={styles.footer}>
-      <p className={styles.footerText}>Copyright 2020 Argent Bank</p>
-    </div>
-  );
-}
